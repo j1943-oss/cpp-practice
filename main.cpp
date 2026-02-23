4da5c5f05036ae527bad6e2184508ec5cd632b13
Justin Kromann <justink1943@gmail.com>
2026-02-23 13:17:29 -0800

    Added loop to print numbers up to user input

--- main.cpp
+++ main.cpp
@@ -8,24 +8,9 @@ int main() {
     cout << "Enter first number: ";
     cin >> a;
 
-    cout << "Enter second number: ";
-    cin >> b;
-
-    int sum = a + b;
-
-    cout << "Sum is: " << sum << endl;
-
-    if (sum % 2 == 0) {
-        cout << "The sum is even." << endl;
-    } else {
-        cout << "The sum is odd." << endl;
+    for (int i=1; i<=a; i++) {
+        cout << i << endl;
     }
-    if (a > b) {
-        cout << "The first number is greater than the second number." << endl;
-    } else if (a < b) {
-        cout << "The second number is greater than the first number." << endl;
-    } else {
-        cout << "Both numbers are equal." << endl;
-    }
+  
     return 0;
 }
