19af1309482488b9998702b85422f08acc9fdb82
Justin Kromann <justink1943@gmail.com>
2026-02-24 18:22:14 -0800

    Added sumUpToN and isEven functions with input validation

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int sumUpToN(int n) {
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+bool isEven(int number) {
+    return number % 2 == 0;
+}
+
 int main() {
     int a;
     int b;
 
-    cout << "Enter first number: ";
+    cout << "Enter N: ";
     cin >> a;
-
-    for (int i=1; i<=a; i++) {
-        cout << i << endl;
-    }
+    while (a < 0) {
+        cout << "Please enter a non-negative integer." << endl;
+        cin >> a;
+    } 
+    cout << "Sum is: " << sumUpToN(a) << endl;
+    cout << "The sum is " << (isEven(sumUpToN(a)) ? "even" : "odd") << endl;
   
     return 0;
+
 }
